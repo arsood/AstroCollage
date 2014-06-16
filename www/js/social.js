aa36058965beda2e844f1314690ddb21c7f6c46f
Melody Serra <melodyserra@gmail.com>
2014-06-16 00:34:31 -0700

    Social plugin

--- www/js/social.js
+++ www/js/social.js
@@ -1,33 +1,4 @@
 $(document).on("tap", "#share-facebook", function(event) {
-    event.preventDefault();
-
-    FB.login(function(response) {
-      if (response.status === 'connected') {
-        
-      }
-    });
-
-    // FB.getLoginStatus(function(response) {
-    //   if (response.status === 'connected') {
-    //     console.log('Logged in.');
-    //   }
-    //   else {
-    //     FB.login();
-    //   }
-    // });
-
-    // FB.api(
-    //     "/me/photos",
-    //     "POST",
-    //     {
-    //         "object": {
-    //             "url": localStorage.getItem("full_canvas_render")
-    //         }
-    //     },
-    //     function (response) {
-    //       if (response && !response.error) {
-    //         console.log(response);
-    //       }
-    //     }
-    // );
+	alert("Worked");
+    window.plugins.socialsharing.shareViaFacebook('Message via Facebook', null, null, function() {console.log('share ok')}, function(errormsg){alert(errormsg)});
 });
